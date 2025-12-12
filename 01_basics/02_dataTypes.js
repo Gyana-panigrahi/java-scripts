89dc55183c5d9311cf865a631a1eca6486d50e43
Gyana-panigrahi <panigrahigyana11@gmail.com>
2025-12-12 15:49:00 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+//Data types in js
+//primitive data types-single value store
+
+//1.Number=>integer+decimal
+//2.bigint=> large number biger then integer value
+//3.String=>character text ""
+//4.boolena=true/fale
+//5.undefined=only variable declare but value not assigned
+//6.null=>intentionaly value not assigned
+//7.symbol= create unique id
+let number=123456; 
+let n=25.45;
+let bigint=56565656555656544654;
+let name="gyana";
+let isvalid=false;
+let x;
+let num=null;
+let id=Symbol("456");
+console.log(number);
+console.log(typeof number);
+console.log(typeof n);
+console.log(typeof bigint);
+console.log(typeof name);
+console.log(typeof isvalid);
+console.log(typeof x);
+console.log(typeof num);
+console.log(typeof id);
+
+//2.Non-primitive Datatypes
+//1.object
+//2.Array
+//3.Function
+
+
+
