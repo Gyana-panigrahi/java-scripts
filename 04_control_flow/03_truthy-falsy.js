88a81555a834c79a1f31f7705581ee17cd75e7a2
Gyana-panigrahi <panigrahigyana11@gmail.com>
2025-12-21 14:02:34 +0000

    nullish-ternary

--- 04_control_flow/03_truthy-falsy.js
+++ 04_control_flow/03_truthy-falsy.js
@@ -23,20 +23,113 @@ if({}){
 //String "false"
 if("false"){
     console.log("Truthy")
-}
-
-const userEmail=[];
-if(userEmail){
-    console.log("Got user Email");
 }else{
-    console.log("don't have user email");
+    console.log("falsy")
+}
+//Empty Array
+const arr=[];
+if(arr.length === 0 ){
+  console.log("array is empty");
+}
+//Empty Object
+const obj={};
+if(Object.keys(obj).length === 0){
+    console.log("Object is Empty");
+}
+//Empty String
+const str="";
+if(!str || str.length === 0){
+    console.log("String is Empty");
 }
 
-if(userEmail.length ===0 ){
-    console.log("Array is Empty");
+//Nullish Coalescing Operator (??): null undefined
+// => it return the right hand operand when left hand operand is null or undefined
+// syntax : val1 ?? val2
+
+// null
+let name=null;
+let userName=name ?? "Guest";
+console.log(userName);
+// undefined
+let age;
+console.log(age ?? 18);//18
+
+//fuction
+function greet(name){
+    let finalName=name ?? "Gyana";
+    console.log(finalName);
 }
-const emptyObj={};
-if(Object.keys(emptyObj).length===0){
-    console.log("object is empty");
-    
+greet("Sai");//sai
+greet(null);//Gyana
+greet(undefined);//Gyana
+
+
+
+// || (logical or)
+let count = 0;
+console.log(count || 10);//10
+
+// ?? nullish coalescing operator
+let counts=0;
+console.log(counts ?? 10);//0
+
+// empty string ("")
+let strs="";
+console.log(strs || "guest");//guest
+
+// nullish (??)
+let strs1="";
+console.log(strs1 ?? "guest");//""
+
+//Ternary operator
+
+//syntax: 
+// condition ? true : false;
+
+const maggie=60;
+maggie <= 40 ? console.log("maggie is less than 40") : console.log
+("maggie is greater  than 40");
+
+let ages=20;
+let result= ages >=18 ? "Adult" : "Minor";
+console.log(result);
+
+let marks=80;
+
+let grade = marks >= 90 ? "A" :
+            marks >= 75 ? "B" :
+            marks >= 60 ? "C" : "fail";
+
+console.log(grade);
+
+//using if else
+let num=20;
+if(num % 2 === 0){
+    console.log("Even");
+}else{
+    console.log("odd");
 }
+
+//using ternary operator
+let num1=55;
+let evenOdd= num1 % 2 === 0 ? "Even" : "odd";
+console.log(evenOdd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
