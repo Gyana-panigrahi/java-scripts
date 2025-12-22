905e1d11ee2c4dd0e5787025e3cafdf53201682b
Gyana-panigrahi <panigrahigyana11@gmail.com>
2025-12-22 02:10:10 +0000

    while_do-while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,19 @@
+let i=1;    //initialization
+while(i<=10){    // condition
+    console.log(i);
+    i+=2;      // updation
+}
+
+let myArr=["flash","batman","superman"];
+let arr=0;
+while(arr < myArr.length){
+    console.log(`My value is: ${myArr[arr]}`);
+    arr++;
+}
+
+//do-while loop
+let score = 1;   //initialization
+do{
+    console.log(`score is: ${score}`);  // updation
+    score++;
+}while(score<=10); // condition
