7dca35d3f41cf88df1d497fcf013f6a9ab9ae109
Gyana-panigrahi <panigrahigyana11@gmail.com>
2026-01-04 15:34:00 +0000

    js-BmiCalcualtor

---
+++ 09_bmiCalculator/index.js
@@ -0,0 +1,15 @@
+const  form=document.querySelector("form");
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  const height=parseInt(document.querySelector("#height").value);
+  const weight=parseInt(document.querySelector("#weight").value);
+  const result=document.querySelector(".result");
+  if(isNaN(height) || height < 0){
+    result.innerHTML=`please input a valid ${height}`
+  }else if(isNaN(weight) || weight< 0){
+    result.innerHTML=`please inut valid ${weight}`;
+  }else{
+     const bmi = (weight / ((height*height)/10000)).toFixed(2);
+    result.innerHTML=`<span>${bmi}</span>`;
+  }
+})
